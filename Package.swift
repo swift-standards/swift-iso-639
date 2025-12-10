87e2a6406ba8918d1f7593eca8d0e197e002a3d1
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 01:16:11 +0100

    Apply swift-format and lint fixes

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-iso-639",
     platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11)
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26)
     ],
     products: [
         .library(name: .iso639, targets: [.iso639]),
